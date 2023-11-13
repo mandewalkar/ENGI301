563649481ce4125380fa7ecce6147ef4115ad14b
Demo User <debian@beaglebone.localdomain>
2023-11-13 21:43:27 +0000

    lots of changes

--- project_01/code/final_code/touch.py
+++ project_01/code/final_code/touch.py
@@ -6,18 +6,95 @@
 import board
 import adafruit_tsc2007
 
-# Use for I2C
-i2c = board.I2C()  # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 
-irq_dio = None  # don't use an irq pin by default
-# uncomment for optional irq input pin so we don't continuously poll the I2C for touches
-# irq_dio = digitalio.DigitalInOut(board.A0)
-tsc = adafruit_tsc2007.TSC2007(i2c, irq=irq_dio)
+class Touchscreen():
+    tsc = None
+    point = None
+    x_coord = None
+    y_coord = None
+    
+    def __init__(self):
+        
+        # Use for I2C
+        i2c = board.I2C()  # uses board.SCL and board.SDA
+        # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 
-while True:
-    if tsc.touched:
-        point = tsc.touch
-        if point["pressure"] < 100:  # ignore touches with no 'pressure' as false
-            continue
-        print("Touchpoint: (%d, %d, %d)" % (point["x"], point["y"], point["pressure"]))
+        irq_dio = None  # don't use an irq pin by default
+        # uncomment for optional irq input pin so we don't continuously poll the I2C for touches
+        # irq_dio = digitalio.DigitalInOut(board.A0)
+        self.tsc = adafruit_tsc2007.TSC2007(i2c, irq=irq_dio)
+        
+    def is_touched(self):
+        if (self.tsc != None):
+            if self.tsc.touched:
+                self.point = self.tsc.touch
+                self.x_coord = self.point["x"]
+                self.y_coord = self.point["y"]
+                if self.point["pressure"] >= 100:  # ignore touches with no 'pressure' as false
+                    return True
+        return False
+        
+    
+    def check_top_right(self):
+        if (self.x_coord < 800 and self.y_coord < 800):
+            return True
+        else:
+            return False
+        
+    def check_top_left(self):
+        if (self.x_coord < 800 and self.y_coord > 3300):
+            return True
+        else:
+            return False
+
+    def check_bottom_right(self):
+        if (self.x_coord > 3300 and self.y_coord < 800):
+            return True
+        else:
+            return False 
+
+    def check_bottom_left(self):
+        if (self.x_coord > 3300 and self.y_coord > 3300):
+            return True
+        else:
+            return False
+
+    def check_middle(self):
+        if (self.x_coord < 2200 and self.x_coord > 1800 and self.y_coord < 2200 and self.y_coord > 1800):
+            return True
+        else:
+            return False
+        
+    def check_quad_1(self):
+        if (self.x_coord < 2000 and self.y_coord < 2000):
+            return True
+        else:
+            return False
+        
+    def check_quad_2(self):
+        if (self.x_coord < 2000 and self.y_coord > 2000):
+            return True
+        else:
+            return False
+
+    def check_quad_3(self):
+        if (self.x_coord > 2000 and self.y_coord > 2000):
+            return True
+        else:
+            return False
+
+    def check_quad_4(self):
+        if (self.x_coord > 2000 and self.y_coord < 2000):
+            return True
+        else:
+            return False
+
+
+touchy = Touchscreen()
+try:
+    while(1):
+        if (touchy.is_touched()):
+            print(touchy.check_quad_4())
+except KeyboardInterrupt:
+    print("Program ended by user.")
+    
